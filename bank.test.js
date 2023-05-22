6d9114cd7ac8b5c6318892fa62575c40360002e7
Sidra Iqbal <114309741+siqbal181@users.noreply.github.com>
2023-05-22 15:41:08 +0100

    Added beforeEach for creating new instance of bank

--- bank.test.js
+++ bank.test.js
@@ -1,8 +1,18 @@
 const Bank = require('./bank');
 
 describe('Bank', () => {
+  let bank ; 
+
+  beforeEach(() => {
+    bank = new Bank();
+  })
+
   it('returns a value of 0 when initialized', () => {
-    const bank = new Bank;
     expect(bank.printBalance()).toBe(0);
   })
+  
+  // it('returns a value of 10 when 10 is deposited', () => {
+  //   bank.deposit(10);
+  //   expect(bank.printBalance()).toBe(10);
+  // })
 })
