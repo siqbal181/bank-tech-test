4bd1d5b8be5c98198771dfeaef5fa691d4c915f2
Sidra Iqbal <114309741+siqbal181@users.noreply.github.com>
2023-05-22 19:51:51 +0100

    Removed old tests which aligned with less matured code

--- bank.test.js
+++ bank.test.js
@@ -11,18 +11,6 @@ describe('Bank', () => {
     expect(bank.printBalance()).toBe(0);
   })
 
-  it('returns a value of 10 when 20 is deposited and 10 is withdrawn', () => {
-    bank.deposit(10);
-    bank.deposit(10);
-    bank.withdraw(10);
-    expect(bank.printBalance()).toBe(10);
-  })
-
-  it('return a balance and date when the amount is deposited with date 14/01/2023', () => {
-    bank.deposit(1000, '14/01/2023');
-    expect(bank.printBalance()).toEqual([{"14/01/2023": 1000}]);
-  })
-
   it('returns balance and date when 1000 deposited on 10/01/2023 and 2000 on 13/01/2023', () => {
     bank.deposit(1000, '10/01/2023');
     bank.deposit(2000, '13/01/2023');
